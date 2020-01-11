909a21663ab4c17017774cac29f68bded185e13d
Juliana <juliana@itsencrypted.com>
2020-01-11 20:08:44 -0300

    stateful widget
    Stateful Widget created

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,46 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
-      MaterialApp(
-        home: null,
+  MaterialApp(
+    home: MagicBall(),
+  ),
+);
+
+class MagicBall extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        backgroundColor: Colors.blue[900],
+        title: Text('Ask me anything'),
+      ),
+      body: AnswerBall(),
+    );
+  }
+}
+
+class AnswerBall extends StatefulWidget {
+  @override
+  _AnswerBallState createState() => _AnswerBallState();
+}
+
+class _AnswerBallState extends State<AnswerBall> {
+
+  int ballnumber = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: FlatButton(
+        onPressed: (){
+          setState(() {
+            ballnumber = Random().nextInt(5) + 1;
+          });
+        },
+        child: Image.asset('images/ball$ballnumber.png'),
       ),
     );
+  }
+}
